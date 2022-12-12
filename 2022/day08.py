ec397c3877e5c8410a3257f33878755930c5f843
Robbie Penziol <rpenziol@users.noreply.github.com>
2022-12-11 23:38:59 -0800

    Day 8 solution

--- 2022/day08.py
+++ 2022/day08.py
@@ -45,4 +45,40 @@ for row in range(num_rows):
             visible_count+=1
             continue
 
-print(visible_count)
+print(visible_count)  # Part 1
+
+
+top_scenic_score = 0
+
+def visible_trees(treehouse_height, trees):
+    visible_tree = []
+
+    for tree in trees:
+        tree_height = int(tree)
+
+        visible_tree.append(tree_height)
+
+        if tree_height >= treehouse_height:
+            break
+
+    return visible_tree
+
+
+for row in range(num_rows):
+    for column in range(num_columns):
+        if row == 0 or column == 0:
+            continue
+
+        treehouse_height = int(matrix[row, column])
+
+        visible_right = visible_trees(treehouse_height, matrix[row, column+1:])
+        visible_left = visible_trees(treehouse_height, reversed(matrix[row, :column]))
+        visible_bottom = visible_trees(treehouse_height, matrix[row+1:, column])
+        visible_up = visible_trees(treehouse_height, reversed(matrix[:row, column]))
+
+        total_scenic_score = len(visible_right) * len(visible_left) * len(visible_bottom) * len(visible_up)
+
+        if total_scenic_score > top_scenic_score:
+            top_scenic_score = total_scenic_score
+
+print(top_scenic_score)  # Part 2
